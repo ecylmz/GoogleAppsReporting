4ef52a51befc313200a80a321fc5e4a85aaf3b1e
emre can yılmaz <ecylmz@bil.omu.edu.tr>
2011-04-22 22:34:38 +0300

    fixed: whiteList error.

--- data.py
+++ data.py
@@ -7,15 +7,15 @@ import os
 import csv
 import datetime
 
-def fetchReport(email,password,date,file):
-    command = "python reporting.py --email=%s --password=%s --report=accounts --date=%s > %s" %(email,password,date,file)
+def fetchReport(email, password, date, file):
+    command = "python reporting.py --email=%s --password=%s --report=accounts --date=%s > %s" %(email, password, date, file)
     os.system(command)
     return file
 
 def lastWeekControl(file):
     wList = whiteList()
 
-    with open(file,"r") as file:
+    with open(file, "r") as file:
         rows = csv.reader(file)
         for row in rows:
             todayDate = row[0][:4] + "-" + row[0][4:6] + "-" + row[0][6:8]
@@ -24,7 +24,7 @@ def lastWeekControl(file):
                 yearDiff = int(todayDate[:4]) - int(lastLoginDate[:4])
                 monthDiff = int(todayDate[5:7]) - int(lastLoginDate[5:7])
                 dayDiff = int(todayDate[8:10]) - int(lastLoginDate[8:10])
-                if yearDiff >= 1 or monthDiff >= 1 or dayDiff >= 6 and row[2] not in wList:
+                if ( yearDiff >= 1 or monthDiff >= 1 or dayDiff >= 6 ) and row[2] not in wList:
                     print "%s adlı kullanıcı son 1 haftadır giriş yapmadı..."%(row[2])
 
 def whiteList():
@@ -45,11 +45,10 @@ def main():
     file = 'data.csv'
 
     try:
-        fetchReport(email,password,date,file)
+        fetchReport(email, password, date, file)
         lastWeekControl(file)
     except:
         print "Hata oluştu,çıkıyorum..."
 
 if __name__ == '__main__':
     main()
-
